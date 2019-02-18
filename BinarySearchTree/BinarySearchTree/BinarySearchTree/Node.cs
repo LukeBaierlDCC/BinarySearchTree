363b77065a59b9b20871244d2e3f036983f2b124
Luke <lukeest1984@gmail.com>
2019-02-17 21:16:18 -0600

    Added a static, non-recursive tree traversal to the Node.

--- BinarySearchTree/BinarySearchTree/BinarySearchTree/Node.cs
+++ BinarySearchTree/BinarySearchTree/BinarySearchTree/Node.cs
@@ -38,6 +38,43 @@ namespace BinarySearchTree
             //}
         }
 
+        public static void PrintTree(Node tree)
+        {
+            List<Node> firstStack = new List<Node>();
+            firstStack.Add(tree);
+
+            List<List<Node>> childListStack = new List<List<Node>>();
+            childListStack.Add(firstStack);
+
+            while (childListStack.Count > 0)
+            {
+                List<Node> childStack = childListStack[childListStack.Count - 1];
+
+                if (childStack.Count == 0)
+                {
+                    childListStack.RemoveAt(childListStack.Count - 1);
+                }
+                else
+                {
+                    tree = childStack[0];
+                    childStack.RemoveAt(0);
+
+                    string indent = "";
+                    for (int i = 0; i < childListStack.Count - 1; i++)
+                    {
+                        indent += (childListStack[i].Count > 0) ? "| " : " ";
+                    }
+
+                    Console.WriteLine(indent + "+- " + tree.Name);
+
+                    if (tree.Children.Count > 0)
+                    {
+                        childListStack.Add(new List<Node>(tree.Children));
+                    }
+                }
+            }
+        }
+
         public Node(object value)
         {
             this.value = value;
@@ -78,18 +115,18 @@ namespace BinarySearchTree
             }
         }
         
-        public void printInOrder()
-        {
-            if (left != null)
-            {
-                left.printInOrder();
-            }
-            System.out.printIn(data);
-            if (right != null)
-            {
-                right.printInOrder();
-            }
-        }
+        //public void printInOrder()
+        //{
+        //    if (left != null)
+        //    {
+        //        left.printInOrder();
+        //    }
+        //    System.out.printIn(data);
+        //    //if (right != null)
+        //    {
+        //        right.printInOrder();
+        //    }
+        //}
 
         private void printIn(int data)
         {
